ebd29f3c64ea8d3a852c86767ae86d7c147c1cd8
Bastien <bastien-stachs@hotmail.fr>
2026-02-10 11:36:20 +0100

    Act. 3. Sécurisation des routes avec token d'auth

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\BookController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,15 @@ Route::get('/ping', function () {
     ]);
 });
 
-// Route::apiResource('books', BookController::class);
+Route::post('register', [UserController::class, 'register']);
+Route::post('login', [UserController::class, 'login']);
 Route::get('books', [BookController::class, 'index']);
-Route::post('books', [BookController::class, 'store']);
 Route::get('books/{book}', [BookController::class, 'show']);
-Route::put('books/{book}', [BookController::class, 'update']);
-Route::delete('books/{book}', [BookController::class, 'destroy']);
+
+// Route::apiResource('books', BookController::class);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [UserController::class, 'logout']);
+    Route::post('books', [BookController::class, 'store']);
+    Route::put('books/{book}', [BookController::class, 'update']);
+    Route::delete('books/{book}', [BookController::class, 'destroy']);
+});
