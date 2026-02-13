fc1feb049ca107ae8c2a96e749ca7188ba5470ea
Bastien <bastien-stachs@hotmail.fr>
2026-02-13 11:00:45 +0100

    test fonctionnel transformé en test unitaire

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -4,26 +4,22 @@ namespace Tests\Unit;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
+use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
-    use RefreshDatabase;
-
     public function test_professional_email_returns_true(): void
     {
-        $user = User::factory()->create([
-            'email' => 'john@entreprise.com'
-        ]);
+        $user = new User();
+        $user->email = 'john@entreprise.com';
 
         $this->assertTrue($user->usesProfessionalEmail());
     }
 
     public function test_gmail_email_returns_false(): void
     {
-        $user = User::factory()->create([
-            'email' => 'john@gmail.com'
-        ]);
+        $user = new User();
+        $user->email = 'john@gmail.com';
 
         $this->assertFalse($user->usesProfessionalEmail());
     }
